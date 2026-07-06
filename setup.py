f919029351900221a19e8a6a8b0af12a5c255bf6
Zsobix <index@zsobix.xyz>
2026-07-06 20:37:55 +0200

    fix: forgor to add a dot :skull:

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="teletext",
-    version="0.0.1",
+    version="0.0.2",
     author="Zsombor Kalmar",
     description="Desktop version of some Teletext services.",
     long_description=long_description,
--- teletext/window.py
+++ teletext/window.py
@@ -1,4 +1,4 @@
-from handler import *
+from .handler import *
 
 import gi
 
